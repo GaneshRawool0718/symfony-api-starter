dc710d2271655b4314769779799ae64e9efa6d9c
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-11-21 17:55:33 +0530

    feat(doctrine): add User entity and database table mapping

---
+++ src/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /** 
+     * @ORM\Id 
+     * @ORM\GeneratedValue(strategy="AUTO") 
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /** @ORM\Column(type="string", length=100) */
+    private $name;
+
+    public function getId() { return $this->id; }
+
+    public function getName(): string { return $this->name; }
+
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+}
